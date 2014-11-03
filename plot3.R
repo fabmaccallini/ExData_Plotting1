193259d01b8887b5d4538d9af20d4692ebaaeb90
fabmaccallini <fabrizio.maccallini@gmail.com>
2014-11-03 16:37:01 +0000

    Add plots

--- plot3.R
+++ plot3.R
@@ -4,8 +4,8 @@
 
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 data.set <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
-rm(data)
-data.set$Global_active_power <- as.numeric(as.character(data.set$Sub_metering_1))
+% rm(data)
+for (i in 7: 9) data.set[, i] <- as.numeric(as.character(data.set[, i]))
 temp <- paste(data.set$Date, data.set$Time)
 data.set$Date <- strptime(temp, format = "%d/%m/%Y %H:%M:%S")
 png(filename = "plot3.png")
@@ -13,6 +13,6 @@ with(data.set, {
     plot(data.set$Date, data.set$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
     lines(data.set$Date, data.set$Sub_metering_2, col = "red")
     lines(data.set$Date, data.set$Sub_metering_3, col = "blue")
-    legend("topright", colnames(data.set)[7: 9], lty = c(1, 1), col=c("black", "red", "blue"))
+    legend("topright", colnames(data.set)[7: 9], lty = c(1, 1), col = c("black", "red", "blue"))
 })
 dev.off()
