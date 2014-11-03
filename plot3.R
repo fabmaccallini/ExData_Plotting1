0e90a26747427a8117a35f231c99a7f375543b8a
fabmaccallini <fabrizio.maccallini@gmail.com>
2014-11-03 16:46:00 +0000

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,10 +1,10 @@
 # Exploratory Data Analysis - Course Project 1
-# Please download and extract the file form 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+# Please download and extract the .txt file form 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
 # into the R working directory before running the code.
 
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 data.set <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
-% rm(data)
+rm(data)
 for (i in 7: 9) data.set[, i] <- as.numeric(as.character(data.set[, i]))
 temp <- paste(data.set$Date, data.set$Time)
 data.set$Date <- strptime(temp, format = "%d/%m/%Y %H:%M:%S")
